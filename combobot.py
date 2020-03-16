df15d17d884cb505d9c35240e2a9d72770a18985
botticket <59758461+botticket@users.noreply.github.com>
2020-03-16 20:08:58 +0700

    Add files via upload

--- combobot.py
+++ combobot.py
@@ -1,3 +1,5 @@
+# https://cryptobott.herokuapp.com/callback
+
 import os
 import sys
 
@@ -56,7 +58,7 @@ def handle_message(event):
     userid = event.source.user_id
     
     disname = line_bot_api.get_profile(user_id=userid).display_name
-    request_text= (' mybot'+'\n' + '>> {} : {}').format(disname,text_from_user)
+    request_text= (' mybot'+'\n' + '>> {} : {}'+'\n' + '>> {}').format(disname,text_from_user,userid)
 
     print(request_text)
     linechat(request_text)
@@ -100,7 +102,7 @@ def handle_message(event):
                     list = self.code
 
                     dfY = data.DataReader(f'{list}', data_source="yahoo", start='2020-01-01', end=end)
-                    dfW = data.DataReader(f'{list}', data_source="yahoo", start='2020-03-09', end=end)
+                    dfW = data.DataReader(f'{list}', data_source="yahoo", start='2020-03-16', end=end)
                     #2020-01-01 = Y M D
 
                     OpenY = dfY['Open'].iloc[1]
@@ -246,7 +248,7 @@ def handle_message(event):
                 end = datetime.now()
                 start = datetime(end.year,end.month,end.day)
                 dfY = data.DataReader('THB=X', data_source="yahoo", start='2020-01-01', end=end)
-                dfW = data.DataReader('THB=X', data_source="yahoo", start='2020-03-09', end=end)
+                dfW = data.DataReader('THB=X', data_source="yahoo", start='2020-03-16', end=end)
                 #2020-01-01 = Y M D
 
                 OpenY = dfY['Open'].iloc[1]
@@ -857,7 +859,7 @@ def handle_message(event):
                     dfQ = data.DataReader(f'{list}', data_source="yahoo", start='2020-01-01', end=end)
                     #chg for Quarter : Jan Apr Jul Sep
 
-                    dfW = data.DataReader(f'{list}', data_source="yahoo", start='2020-03-09', end=end)
+                    dfW = data.DataReader(f'{list}', data_source="yahoo", start='2020-03-16', end=end)
                     #2020-01-01 = Y M D
 
                     list = list.replace('.bk','')
