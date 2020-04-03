a3a713f1e73f8690d82a19c34a6599f8ca0ba71f
botticket <59758461+botticket@users.noreply.github.com>
2020-04-03 11:21:07 +0700

    Add files via upload

--- combobot.py
+++ combobot.py
@@ -960,7 +960,7 @@ def handle_message(event):
                     ChgM = '%.2f'%ChgM
                     ChgM = float(ChgM)	
                     
-                    text1 = exit1 + ' | ' + exit2 + ' | ' + exit3 + '\n'  + 'm {} : H {}'.format(OpenM,max_value)
+                    text1 = exit1 + ' | ' + exit2 + ' | ' + exit3 + '\n' + 'y {} : m {} : H {}'.format(OpenY,OpenM,max_value)
                     text2 = support1 + ' | ' + support2 + ' | ' + support3 + '\n' + 'y {} : m {} : L {}'.format(OpenY,OpenM,min_value)
 
                     alert1 = 'ชนแนวต้าน'
