b4dc59238eb4b88a576832b5a40b5d23e16ed1c0
botticket <59758461+botticket@users.noreply.github.com>
2020-06-19 09:48:11 +0700

    Add files via upload

--- combobot.py
+++ combobot.py
@@ -40,7 +40,7 @@ else:
 
 def linechat(text):
     
-    ACCESS_TOKEN = "oK2sk4w1eidfRyOVfgIcln38TBS8JmL0PgfbbQ8t0Zv"
+    ACCESS_TOKEN = "qh4YLKs18Z4RYKLvsFnLmgtVWmSpi7pY7KS112AFl7C"
 
     notify = LineNotify(ACCESS_TOKEN)
 
